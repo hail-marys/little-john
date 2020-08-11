4a117483e6c06498375c4934e6514834bc921b18
Richard Whitehead <damian253@me.com>
2020-08-10 18:34:42 -0700

    bot switch

---
+++ little_john/trade_bot.py
@@ -0,0 +1,20 @@
+import sys
+import finnhub
+
+class Trade_Bot:
+    """turns tradebot on and off, default initialization is off 
+    """
+    
+    def __init__(self):
+        self.status = 'off'
+        
+    def turn_on_or_off(self):    
+      """ask if you want bot on or off
+      """  
+      if self.status == 'off':
+        self.status = 'on'
+      if self.status == 'on':
+        self.status = 'off'  
+        
+if __name__ == "__main__":
+    print('off')
