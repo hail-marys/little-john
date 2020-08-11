04c6ccad95c8ead0a9930b52e31ba1c25bb0eec4
Richard Whitehead <damian253@me.com>
2020-08-10 19:05:34 -0700

    tradebot complete

--- little_john/trade_bot.py
+++ little_john/trade_bot.py
@@ -9,12 +9,10 @@ class Trade_Bot:
         self.status = 'off'
         
     def turn_on_or_off(self):    
-      """ask if you want bot on or off
+      """ask if you want to turn bot on or off
       """  
       if self.status == 'off':
         self.status = 'on'
       if self.status == 'on':
         self.status = 'off'  
         
-if __name__ == "__main__":
-    print('off')
