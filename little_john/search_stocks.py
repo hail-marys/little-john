400eecc64e592a8047f946913a2f11b3c5884977
natalie sinner <nataliesinner@hotmail.com>
2020-08-13 18:34:45 -0700

    finished tests for search stocks

--- little_john/search_stocks.py
+++ little_john/search_stocks.py
@@ -54,48 +54,57 @@ class SearchStocks:
         """Gets highest stock price of the day"""
         high = self.client.quote(symbol)['h']
         print(f'High: {high}')
+        return f'High: {high}'
 
 
     def low_stock_price(self, symbol):
         """Gets lowest stock price"""
         low = self.client.quote(symbol)['l']
         print(f'Low: {low}')
+        return f'Low: {low}'
 
 
     def market_cap(self, symbol):
         """Gets market cap for company"""
         cap = self.client.company_profile(symbol=symbol)['marketCapitalization']
         print(f'Market Capitalization: {cap}')
+        return f'Market Capitalization: {cap}'
 
 
     def week_high(self, symbol):
         """Gets 52-week high"""
         high = self.client.company_basic_financials(symbol, 'all')['metric']['52WeekHigh']
         print(f'52 week high: {high}')
+        return f'52 week high: {high}'
 
 
     def week_low(self, symbol):
         """Gets 52-week low"""
         low = self.client.company_basic_financials(symbol, 'all')['metric']['52WeekLow']
         print(f'52 week low: {low}')
+        return f'52 week low: {low}'
 
 
-    def pe_ration(self, symbol):
+    def pe_ratio(self, symbol):
         """Get P/E ratio trailing twelve months"""
         ratio = self.client.company_basic_financials(symbol, 'all')['metric']['pfcfShareTTM']
         print(f'P/E ratio: {ratio}')
+        return f'P/E ratio: {ratio}'
 
 
     def div_yield(self, symbol):
         """Get annual div yield"""
         div = self.client.company_basic_financials(symbol, 'all')['metric']['dividendYieldIndicatedAnnual']
         print(f'DIV yield: {div}')
+        return f'DIV yield: {div}'
 
     
     def candle(self, symbol):
         """Candle stick data for bot, pulled data from each day between 8/12/2019 1pm - 8/12/2020 1pm"""
         candle = self.client.stock_candles(symbol, 'D', 1565640000, 1597262400)
         # print(f'{candle}')
+        return f'{candle}'
+
 
 
 
@@ -107,7 +116,7 @@ search.low_stock_price('AAPL')
 search.market_cap('AAPL')
 search.week_high('AAPL')
 search.week_low('AAPL')
-search.pe_ration('AAPL')
+search.pe_ratio('AAPL')
 search.div_yield('AAPL')
 """For bot function"""
 # search.candle('AAPL') 
