97c1e25b6529ced90a8882b7285aee2b09c23b4b
Richard Whitehead <damian253@me.com>
2020-08-15 09:01:24 -0700

    made changes to trade history

--- little_john/trade_history.py
+++ little_john/trade_history.py
@@ -7,38 +7,24 @@ class View_Trade_History:
     def __init__(self, file):
         with open(file, 'r') as history:
             trade_history = json.load(history)
-
-            self.name = trade_history['name']
-            self.symbol = trade_history['symbol']
-            self.amount = trade_history['amount']
-            self.number_of_shares = trade_history['shares']
-            self.start_date = trade_history['date_purchased']
+            self.data = trade_history
             print(trade_history)
-            
-            
-    
-    def __repr__(self):
-        """This method returns the string representation of the object
-        """
-        return f'"name": {self.name}, "symbol": {self.symbol},"amount": {self.amount},"shares": {self.number_of_shares},"date_purchased": {self.start_date}'
 
     def display_history(self):
         """
         Display data will be pulled from json file
         """
-        for i in range(len(self.name)):
-            print(f'\nName: {self.name[i]}')
-            print(f'Symbol: {self.symbol[i]}')
-            print(f'Amount: {self.amount[i]}')
-            print(f'Shares: {self.number_of_shares[i]}')
-            print(f'Date_Purchased: {self.start_date[i]}\n')
+        for i in range(len(self.data)):
+            print(f'\nName: {self.data[i]["name"]}')
+            print(f'Symbol: {self.data[i]["symbol"]}')
+            print(f'TypeOfBuy: {self.data[i]["typeOfBuy"]}')
+            print(f'Invested: {self.data[i]["invested"]}')
+            print(f'Shares: {self.data[i]["shares"]}')
+            print(f'Date_Purchased: {self.data[i]["name"]}\n')
 
 if __name__ == "__main__":
     test = View_Trade_History('./logs/trade_history.json')
-    test.display_history()
     
-
-
         
         
     
