ad500dcc57880a8ceb396a6b4e2268bc88ab33a8
natalie sinner <nataliesinner@hotmail.com>
2020-08-15 10:19:23 -0700

    search stock complete

--- little_john/search_stocks.py
+++ little_john/search_stocks.py
@@ -1,39 +1,40 @@
 import finnhub
 import requests
 
-# finnhub_client = finnhub.Client(api_key='brqm9efrh5rce3ls8mdg')
-
-# r = requests.get('https://finnhub.io/api/v1/quote?symbol=AAPL&token=')
-# print(r.json())
-
-
-def start():
-    run = True
-    while run:
-        display_text('AAPL')
-        ipt = input('> ')
-        if ipt == 'quit':
-            sys.exit('main_menu')
-        else:
-            print('Please search for a company')
-
-
-def display_text(symbol):
-    print(f'Which company would you like to search for? Enter symbol:')
-
-
-# start()
-
 
 class SearchStocks:
 
     def __init__(self):
+        """Hold API key"""
         self.client = finnhub.Client(api_key='brqm9efrh5rce3ls8mdg')
 
 
     def menu(self):
-        entry = input(f'Which company would you like to search for? Enter symbol: ')
-        self.current_stock_price(entry)
+        """Allows user to enter a company and displays values"""
+        entry = input('Which company would you like to search for? Enter symbol:\n').upper() 
+        if self.check(entry):
+            self.current_stock_price(entry)
+            self.open_stock_price(entry)
+            self.high_stock_price(entry)
+            self.low_stock_price(entry)
+            self.market_cap(entry)
+            self.week_high(entry)
+            self.week_low(entry)
+            self.pe_ratio(entry)
+            self.div_yield(entry)
+            i = input('Press any key to return to the main menu')
+        else:
+            print(f'That company symbol does not exist.\n')
+            self.menu()
+
+
+    def check(self, entry):
+        """Checks to see if company entered is valid"""
+        data = self.client.company_profile(symbol=entry)
+        if data == {}:
+            return False 
+        else:
+            return True 
 
 
     def current_stock_price(self, symbol):
@@ -41,7 +42,7 @@ class SearchStocks:
         current = self.client.quote(symbol)['c']
         print(f'Current Stock Value: {current}')
         return f'Current Stock Value: {current}' 
-    
+
 
     def open_stock_price(self, symbol):
         """Gets stock price at open"""
@@ -106,17 +107,16 @@ class SearchStocks:
         return f'{candle}'
 
 
-
-
 search = SearchStocks()
-search.current_stock_price('AAPL')
-search.open_stock_price('AAPL')
-search.high_stock_price('AAPL')
-search.low_stock_price('AAPL')
-search.market_cap('AAPL')
-search.week_high('AAPL')
-search.week_low('AAPL')
-search.pe_ratio('AAPL')
-search.div_yield('AAPL')
+# search.menu()
+# search.current_stock_price('AAPL')
+# search.open_stock_price('AAPL')
+# search.high_stock_price('AAPL')
+# search.low_stock_price('AAPL')
+# search.market_cap('AAPL')
+# search.week_high('AAPL')
+# search.week_low('AAPL')
+# search.pe_ratio('AAPL')
+# search.div_yield('AAPL')
 """For bot function"""
 # search.candle('AAPL') 
